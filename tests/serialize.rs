39b1994b4bc664018061255b59eedbb829f47677
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-27 13:00:52 -0400

    Refactor the tests

--- tests/serialize.rs
+++ tests/serialize.rs
@@ -1,10 +1,21 @@
 use hdf5::{Encoder, File};
 use rustc_serialize::Encodable;
-use setup;
+use temporary::Directory;
+
+macro_rules! test(
+    ($($name:ident := $value:expr,)*) => ({
+        let directory = Directory::new("hdf5").unwrap();
+        let file = File::new(directory.join("data.h5")).unwrap();
+        $({
+            let mut encoder = Encoder::new(&file, stringify!($name)).unwrap();
+            $value.encode(&mut encoder).unwrap();
+        })*
+    });
+);
 
 #[test]
 fn encode_compound() {
-    let directory = setup();
+    let directory = Directory::new("hdf5").unwrap();
     let file = File::new(directory.join("data.h5")).unwrap();
 
     #[derive(RustcEncodable)]
@@ -41,78 +52,54 @@ fn encode_compound() {
 
 #[test]
 fn encode_scalar() {
-    let directory = setup();
-    let file = File::new(directory.join("data.h5")).unwrap();
+    test!(
+        a := 42f32,
+        b := 42f64,
 
-    macro_rules! test(
-        ($name:expr, $value:expr) => ({
-            let mut encoder = Encoder::new(&file, $name).unwrap();
-            $value.encode(&mut encoder).unwrap();
-        });
+        c := 42i8,
+        d := 42u8,
+
+        e := 42i16,
+        f := 42u16,
+
+        g := 42i32,
+        h := 42u32,
+
+        i := 42i64,
+        j := 42u64,
+
+        k := 42isize,
+        l := 42usize,
     );
-
-    test!("a", 42f32);
-    test!("b", 42f64);
-
-    test!("c", 42i8);
-    test!("d", 42u8);
-
-    test!("e", 42i16);
-    test!("f", 42u16);
-
-    test!("g", 42i32);
-    test!("h", 42u32);
-
-    test!("i", 42i64);
-    test!("j", 42u64);
-
-    test!("k", 42isize);
-    test!("l", 42usize);
 }
 
 #[test]
 fn encode_text() {
-    let directory = setup();
-    let file = File::new(directory.join("data.h5")).unwrap();
-
-    macro_rules! test(
-        ($name:expr, $value:expr) => ({
-            let mut encoder = Encoder::new(&file, $name).unwrap();
-            $value.encode(&mut encoder).unwrap();
-        });
+    test!(
+        a := '界',
+        b := "Hello, 世界!",
     );
-
-    test!("a", '界');
-    test!("b", "Hello, 世界!");
 }
 
 #[test]
 fn encode_vector() {
-    let directory = setup();
-    let file = File::new(directory.join("data.h5")).unwrap();
+    test!(
+        a := vec![42f32, 69f32],
+        b := vec![42f64, 69f64],
 
-    macro_rules! test(
-        ($name:expr, $value:expr) => ({
-            let mut encoder = Encoder::new(&file, $name).unwrap();
-            $value.encode(&mut encoder).unwrap();
-        });
+        c := vec![42i8, 69i8],
+        d := vec![42u8, 69u8],
+
+        e := vec![42i16, 69i16],
+        f := vec![42u16, 69u16],
+
+        g := vec![42i32, 69i32],
+        h := vec![42u32, 69u32],
+
+        i := vec![42i64, 69i64],
+        j := vec![42u64, 69u64],
+
+        k := vec![42isize, 69isize],
+        l := vec![42usize, 69usize],
     );
-
-    test!("a", vec![42f32, 69f32]);
-    test!("b", vec![42f64, 69f64]);
-
-    test!("c", vec![42i8, 69i8]);
-    test!("d", vec![42u8, 69u8]);
-
-    test!("e", vec![42i16, 69i16]);
-    test!("f", vec![42u16, 69u16]);
-
-    test!("g", vec![42i32, 69i32]);
-    test!("h", vec![42u32, 69u32]);
-
-    test!("i", vec![42i64, 69i64]);
-    test!("j", vec![42u64, 69u64]);
-
-    test!("k", vec![42isize, 69isize]);
-    test!("l", vec![42usize, 69usize]);
 }
